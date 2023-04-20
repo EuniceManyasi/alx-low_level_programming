4ebd94316dff6bad41d0100389292af4c2fd22bc
EuniceManyasi <eunismanyasi@gmail.com>
2023-04-20 11:31:44 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+include < stdio.h >
+/**
+ * main - Outputs hello using puts command
+ * Return: 0 (The output is okay)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
