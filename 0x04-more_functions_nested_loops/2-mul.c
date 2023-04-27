f1306db7cb3706b4a983239a42e499ee19015846
EuniceManyasi <eunismanyasi@gmail.com>
2023-04-27 06:17:05 -0700

    First 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiply two integers
+ * @first_number: first integer
+ * @second_number: second integer
+ * Return: value after multiplication
+ */
+
+int mul(int first_number, int second_number)
+{
+	return(first_number * second_number);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print numbers- print 0-9
+ * return: void
+ */
+
+void print_numbers(void)
+{
+	char num;
+	for(num='0'; num<='9'; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
