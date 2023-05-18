71a61d78a6a0b6a2076d138cdad9d0b7d3660290
EuniceManyasi <eunismanyasi@gmail.com>
2023-05-18 12:12:13 -0700

    Malloc More

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,8 +4,8 @@
 /**
  * *_memset - fills memory with a constant byte
  * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
+ * @b_char_to_copy: char to copy
+ * @no_of_copies: number of times to copy b
  *
  * Return: pointer to the memory area s
  */
