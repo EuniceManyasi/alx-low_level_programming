b23fab6d404e02d8ffdd994abd6fab3ac27feae1
EuniceManyasi <eunismanyasi@gmail.com>
2023-05-18 12:06:39 -0700

    Malloc More

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *string_nconcat - concatenates n bytes of a string to another string
+ * @str1: string to append to
+ * @str2: string to concatenate from
+ * @n: number of bytes from str2 to concatenate to str1
+ *
+ * Return: pointer to the resulting string
+ */
+char *string_nconcat(char *str1, char *str2, unsigned int n)
+{
+	char *ptr_s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+
+	while (str1 && str1[len1])
+		len1++;
+	while (str2 && str2[len2])
+		len2++;
+
+	if (n < len2)
+		ptr_s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		ptr_s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!ptr_s)
+		return (NULL);
+
+	while (i < len1)
+	{
+		ptr_s[i] = s1[i];
+		i++;
+	}
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		ptr_s[i++] = s2[j++];
+
+	ptr_s[i] = '\0';
+
+	return (ptr_s);
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b_char_to_copy, unsigned int no_of_copies)
+{
+	unsigned int i;
+
+	for (i = 0; i < no_of_copies; i++)
+	{
+		s[i] = b_char_to_copy;
+	}
+
+	return (s);
+}
+
+/**
+ * *_calloc - allocates memory for an array
+ * @num_array_elements: number of elements in the array
+ * @size_of_each_el: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int num_array_elements, unsigned int size_of_each_el)
+{
+	char *ptr;
+
+	if (num_array_elements == 0 || size_of_each_el == 0)
+		return (NULL);
+
+	ptr = malloc(size_of_each_el * num_array_elements);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	_memset(ptr, 0, num_array_elements * size_of_each_el);
+
+	return (ptr);
+}
