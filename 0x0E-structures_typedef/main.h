c0031637595b1508226195324533c69094481918
EuniceManyasi <eunismanyasi@gmail.com>
2023-05-22 19:42:34 -0700

    structs

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int _putchar(char c);
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+void free_dog(dog_t *d);
+
+#endif
